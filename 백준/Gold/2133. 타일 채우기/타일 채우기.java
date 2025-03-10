9efd7d733d21ad81e8dd398e34112e2d3c7d2aa8
UG HONGSEOHYEON <127842244+xyz987164@users.noreply.github.com>
2025-03-10 11:12:01 +0900

    [Gold IV] Title: 타일 채우기, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -8,18 +8,13 @@ public class Main{
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N+1];
         
-        if (N < 2) {
-            System.out.println(N == 0 ? 1 : 0);
+        if (N % 2 != 0) {
+            System.out.println(0);
             return;
         }
         
         dp[0]=1;
         dp[2]=3;
-        
-        if (N % 2 != 0) {
-            System.out.println(0);
-            return;
-        }
         
         for(int i=4; i<=N; i+=2){
             dp[i]=dp[i-2]*dp[2];
