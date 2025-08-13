bcea0d6f4ef49ae878ef7a6445fe1483aa0b5d5f
UG HONGSEOHYEON <127842244+SH38038038@users.noreply.github.com>
2025-08-13 10:49:19 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 100 ms, Memory: 21988 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[N+1];
+        if (N >= 1) dp[1] = 0;
+        if (N >= 2) dp[2] = 1;
+        
+        for(int i=3; i<=N; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        long answer = dp[N]%1000000007;
+        
+        System.out.println(answer);
+            
+    }
+}
