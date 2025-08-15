6228746badfb40004ce03a0948208159fd8acbcb
UG HONGSEOHYEON <127842244+SH38038038@users.noreply.github.com>
2025-08-15 12:49:20 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            boolean isValid = true;
+
+            for (int j = 0; j < str.length(); j++) {
+                char now = str.charAt(j);
+
+                if (now == '(') {
+                    stack.push(now);
+                } else { 
+                    if (stack.isEmpty()) { 
+                        isValid = false;
+                        break;
+                    }
+                    stack.pop();
+                }
+            }
+            if (isValid && stack.isEmpty()) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
