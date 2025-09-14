3af14c297fae1edf5d1ef4ee3f123aa62e6aa3ff
UG HONGSEOHYEON <127842244+SH38038038@users.noreply.github.com>
2025-09-14 14:12:31 +0900

    [Silver IV] Title: 스택, Time: 276 ms, Memory: 18756 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,47 +1,45 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
         Deque<Integer> stack = new ArrayDeque<>();
-
-        for (int i = 0; i < n; i++) {
+        
+        for(int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             String command = st.nextToken();
-
-            switch (command) {
+            
+            switch(command){
                 case "push":
                     int x = Integer.parseInt(st.nextToken());
                     stack.push(x);
                     break;
-
                 case "pop":
-                    if (stack.isEmpty()) {
+                    if(stack.isEmpty()){
                         System.out.println(-1);
+                        break;
                     } else {
                         System.out.println(stack.pop());
+                        break;
                     }
-                    break;
-
-                case "size":
-                    System.out.println(stack.size());
-                    break;
-
+                case "top":
+                    if(stack.isEmpty()){
+                        System.out.println(-1);
+                        break;
+                    } else {
+                        System.out.println(stack.peek());
+                        break;
+                    }
                 case "empty":
                     System.out.println(stack.isEmpty() ? 1 : 0);
                     break;
-
-                case "top":
-                    if (stack.isEmpty()) {
-                        System.out.println(-1);
-                    } else {
-                        System.out.println(stack.peek());
-                    }
+                case "size":
+                    System.out.println(stack.size());
                     break;
-
+               
                 default:
                     break;
             }
