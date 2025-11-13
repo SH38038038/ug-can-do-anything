3d67e064f4933f2d9574c4c7287c99760cf1c672
UG HONGSEOHYEON <127842244+SH38038038@users.noreply.github.com>
2025-11-13 14:17:59 +0900

    [level 2] Title: 프로세스, Time: 1.27 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,38 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-
         int answer = 0;
-        int l = location; 
-
-        Queue<Integer> que = new LinkedList<Integer>();
-        for (int i : priorities) {
-            que.add(i);
+        int l = location;
+        Queue<Integer> q = new LinkedList<Integer>();
+        for(int i : priorities){
+            q.add(i);
         }
-
         Arrays.sort(priorities);
-        int size = priorities.length - 1;
-
-        while (!que.isEmpty()) {
-            Integer i = que.poll();
-
-            if (i == priorities[size - answer]) {
-                answer++; 
-                l--;     
-                if (l < 0) {
-                    break; 
-                }
-
+        int size = priorities.length-1;
+        
+        while(!q.isEmpty()){
+            int i = q.poll();
+            if(i == priorities[size-answer]){
+                answer++;
+                l--;
+                if(l<0) break;
             } else {
-                
-                que.add(i); 
-                l--; 
-                if (l < 0) {
-                    l = que.size() - 1; 
+                q.add(i);
+                l--;
+                if (l<0){
+                    l = q.size()-1;
                 }
             }
-        }
-
+        } 
         return answer;
     }
 }
